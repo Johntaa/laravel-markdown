a54328797e00980e7f26146a002a4b27ed8a760d
johntaa <fccffccffccf@yahoo.com>
2014-03-26 09:38:26 -0700

    larave 4.1 markdown extra

---
+++ src/Johntaa/Markdown/MarkdownServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Johntaa\Markdown;
+
+use Illuminate\Support\ServiceProvider;
+
+class MarkdownServiceProvider extends ServiceProvider {
+
+	/**
+	* Indicates if loading of the provider is deferred.
+	*
+	* @var bool
+	*/
+	protected $defer = false;
+
+	
+	
+	public function boot(){ 
+     
+      $this->package('johntaa/markdown');
+        
+     
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	* Register the service provider.
+	*
+	* @return void
+	*/
+	public function register()
+	{
+		list($app, $view) = array($this->app, $this->app['view']);
+
+		$app->singleton('markdown', '\Johntaa\Markdown\Markdown');
+
+		$view->addExtension('md', 'markdown', function() use ($app) {
+			return new MarkdownEngine($app['markdown']);
+		});
+
+		 }
+		
+ 
+		/**
+	* Get the services provided by the provider.
+	*
+	* @return array
+	*/
+		public function provides()
+		{
+			return array('markdown');
+		}
+
+	}
